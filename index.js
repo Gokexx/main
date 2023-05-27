a2c360ff256094d4a6835a82161f20c3d71be891
lyronl <lyronlumpkin@gmail.com>
2023-05-26 22:06:39 -0400

    console todo list

---
+++ index.js
@@ -0,0 +1,31 @@
+
+//if this or that is true then
+//if (this || that) {
+    //prompt("Anything Else")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let list = document.getElementById("unlist");
+let input = '';
+for (i = 0; i < input.length; i++) {
+    var li = document.createElement('li');
+    li.innerText = input[i];
+    list.appendChild(li);
+} */
